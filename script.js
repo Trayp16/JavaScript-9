f4570a60cd013026af131abb7ca02e3fdd52e4d8
Trayp16 <xKingRavenx20@gmail.com>
2025-04-01 09:54:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function showRandom(){
+    document.getElementById("output").innerText=Math.random();
+}
+
+function showFloor() {
+    document.getElementById("output").innerText=Math.floor(7.9);
+}
+
+function showCeil() {
+    document.getElementById("output").innerText=Math.ceil(2.2);
+}
+
+function rollDice() {
+    let roll=Math.floor(Math.random()*6)+1;
+    document.getElementById("output").innerText="Dice Roll:"+roll;
+} 
+
+function pickRandom(){
+    let names=["Erica", "Chris", "Taylor", "Jordan"];
+    let index=Math.floor(Math.random()*names.length);
+
+    document.getElementById("output").innerText="Chosen:" + names [index];
+}
